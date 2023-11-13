2370007c769de0a430cc055ef1a9ae12ec1bce05
Hugo <hugo.aguilerac06@gmail.com>
2023-11-12 22:26:53 -0300

    posible fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import  express  from "express";
-const app = express(); //tener una instancia de express para hacer uso de la libreria
-import userRoutes from './src/routes/users.routes.js'
-import roleRoutes from './src/routes/roles.routes.js'
-import authRoutes from './src/routes/auth.routes.js'
-import cookieParser from "cookie-parser";
-import cors from 'cors'
+import express from 'express';
+import userRoutes from './src/routes/users.routes.js';
+import roleRoutes from './src/routes/roles.routes.js';
+import authRoutes from './src/routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
+const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -15,9 +15,14 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/roles', roleRoutes)
+app.use('/api/roles', roleRoutes);
 
+app.get('/', (req, res) => {
+    // Establecer una cookie con SameSite=Lax
+    res.cookie('miCookie', 'miValor', { sameSite: 'Lax' });
+    res.send('Cookie establecida correctamente');
+});
 
 export default app;
